7d4aa83f070676fe2e356d5b851d65d4adbd7249
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2025-03-29 12:47:08 +0100

    Aggiornamenti significativi per l'app .NET con Avalonia
    Modificato il file `.gitignore` per gestire file temporanei.
    Creato `Engine.csproj` con .NET 9.0 e usi impliciti.
    Aggiunta la classe `Player` e relative classi per gestire le caratteristiche del giocatore.
    Creato `app.manifest` per la compatibilità su Windows.
    Aggiornato `Program.cs` per avviare l'applicazione Avalonia.
    Modificato `UnnamedGame.Desktop.csproj` per includere riferimenti necessari.
    Aggiunta descrizione nel `README.md` per il progetto in sviluppo.

---
+++ Models/Player.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+
+namespace Engine.Models;
+
+public class Player(string name)
+{
+    public string Name
+    {
+        get => name;
+    }
+
+    public Job CharacterClass { get; }
+    public Race CharacterRace { get; }
+    public Attributes CharacterAttributes { get; } = new();
+    public int Hitpoints { get; }
+    public int Luck { get; }
+    public int ExperiencePoint { get; }
+    public int Level { get; }
+    public int Gold { get; }
+}
+
+public class Attributes
+{
+    public int Body { get; }
+    public int Mind { get; }
+    public int Wits { get; }
+    public int Reflex { get; }
+}
+
+public enum Job
+{
+    Fighter,
+    Gunner,
+    Scavenger,
+    Technician,
+    Psyker,
+}
+
+public enum Race
+{
+    Human,
+    Android,
+    Cyborg,
+    Mutant,
+    Xeno,
+}
